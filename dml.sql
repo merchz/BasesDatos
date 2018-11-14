c24cf5ee7616a6368189ff777c996e66787cf633
merchz <manuelenrique.ruiz.chimborazo.tubalcain.net>
2018-11-14 21:24:42 +0100

    sentencias para crear base de datos y tablas

--- dml.sql
+++ dml.sql
@@ -10,3 +10,13 @@ show databases;
 use mysql;
 -- para utilizar base de datos
 
+mysql> GRANT ALL ON nombre-bdd.* TO 'nombre-usuario'@'localhost-ip';
+--crear base de datos
+CREATE DATABASE coches;
+
+SHOW TABLES;
+-- ver registro tablas
+select *from nombre_tabla;
+select user, host from user;
+-- crear tabla nombretipo, nombreTIpo,
+create table pet (name varchar(20), owner varchar(20), specie varchar(20), sex char(1), birth date, death date);
